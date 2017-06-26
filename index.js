857acf8e64a60a3a14d387745602307385dda223
sebastian susanto <ssusanto@iu.edu>
2017-06-26 16:40:26 -0400

    Change the heading when form is submitted

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function changeHeading(ev) {
-    ev.target.textContent= 'clickity click'
+    ev.preventDefault()
+    const f = ev.target
+    const name = f.personName.value
+    const heading = document.querySelector('h1')
+    heading.textContent= name 
 }
 
 
-const btn = document.querySelector('button')
-btn.addEventListener('click', changeHeading)
-
-const p = document.querySelector('#instructions')
-p.addEventListener('click', changeHeading)
+const personForm = document.querySelector('#person-form')
+personForm.addEventListener('submit', changeHeading)
