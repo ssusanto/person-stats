261a0e4cb0e34688fb033e90d3e4169a8bffb523
sebastian susanto <ssusanto@iu.edu>
2017-06-27 10:32:45 -0400

    Hw 1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function changeHeading(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const heading = document.querySelector('h1')
-    heading.textContent= name 
+function display(ev) {
+	ev.preventDefault()
+  	const name = document.querySelector('#input1')
+  	const room = document.querySelector ('#input2')
+    const answer = document.querySelector('#result')
+    answer.textContent= name.value + ' lives in University Hall room number ' +room.value
 }
 
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeHeading)
+personForm.addEventListener('submit', display)
