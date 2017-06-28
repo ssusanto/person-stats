500670fef5831a9550eff09c7125cc0e2226f152
sebastian susanto <ssusanto@iu.edu>
2017-06-27 20:58:57 -0400

    Change with CSS layout, Added color option and create a list for answer

--- index.js
+++ index.js
@@ -3,8 +3,40 @@ function display(ev) {
     const form = ev.target
   	const name = form.personName.value
   	const room = form.personRoom.value
+    const color = form.favoriteColor.value
+
+
+    
     const answer = document.querySelector('#result')
-    answer.textContent= name + ' lives in University Hall room number ' +room
+    
+    const list = document.createElement('ul')
+    
+    const nameValue = document.createElement('li')
+    nameValue.textContent = `Name: ${name}`
+    list.appendChild(nameValue)
+    
+    const roomValue = document.createElement('li')
+    roomValue.textContent = `Room Number: ${room}`
+    list.appendChild(roomValue)
+    
+    const colorItem = document.createElement('li')
+    colorItem.textContent = 'Favorite Color: '
+    colorItem.style.width = '6rem'
+    colorItem.style.height = '3rem'
+
+    
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+    colorItem.appendChild(colorDiv)
+    
+    list.appendChild(colorItem)
+    
+    answer.appendChild(list)
+
+    //answer.textContent= name.value + ' lives in University Hall room number ' +room.value
+
 }
 
 
